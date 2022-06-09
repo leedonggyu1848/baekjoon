a3280436c3c0f986ee77c4c6745cdbf5f1ebbd58
donggyu <53461960+leedonggyu1848@users.noreply.github.com>
2022-06-09 16:13:02 +0900

    Update 연산자 끼워넣기.cc

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,70 +1,60 @@
-#include<iostream>
-#include<algorithm>
-#include<iterator>
-#include<string>
-#include<cstring>
-#include<array>
-#include<vector>
-#include<deque>
-#include<queue>
-#include<stack>
-#include<cstdio>
-#include<iomanip>
-#include<fstream>
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-int n;
-int num[11];
-int opt[4];
-int maxv = -100000001;
-int minv = 100000001;
+#define MAXN 11
+#define NUM_OPERATORS 4
+#define INF 987654321
+#define FOR(N) for(int i=0; i<(N); ++i)
 
-void solve(int rst, int pos)
-{
-	if (pos == (n-1))
-	{
-		maxv = max(maxv, rst);
-		minv = min(minv, rst);
-		return;
-	}
-	for (int i = 0; i < 4; i++)
-	{
-		if (opt[i] != 0) {
-			opt[i]--;
-			switch (i)
-			{
-			case 0:
-				solve(rst + num[pos+1], pos + 1);
-				break;
-			case 1:
-				solve(rst - num[pos+1], pos + 1);
-				break;
-			case 2:
-				solve(rst * num[pos+1], pos + 1);
-				break;
-			case 3:
-				solve(rst / num[pos+1], pos + 1);
-				break;
-			default:
-				break;
-			}
-			opt[i]++;
-		}
-	}
+int gN;
+int gSeq[MAXN];
+int gOpCnt[4];
+
+enum { ADD=0, SUB, MUL, DIV };
+enum { MAXIMUM=0, MINIMUM };
+
+int _add(int a, int b) { return a+b; }
+int _sub(int a, int b) { return a-b; }
+int _mul(int a, int b) { return a*b; }
+int _div(int a, int b) { return a/b; }
+
+int(*gOperators[NUM_OPERATORS])(int,int) = {
+  _add, _sub, _mul, _div
+};
+
+void getInput() {
+  scanf("%d\n", &gN);
+  FOR(gN) scanf("%d\n", &gSeq[i]);
+  FOR(NUM_OPERATORS) scanf("%d\n", &gOpCnt[i]);
 }
 
-int main()
-{
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%d", &num[i]);
-	}
-	for (int i = 0; i < 4; i++)
-	{
-		scanf("%d", &opt[i]);
-	}
-	solve(num[0], 0);
-	printf("%d\n%d", maxv, minv);
-	return 0;
+void findAnswer(int answer[2], int cur, int idx) {
+  if (idx == gN){
+    answer[MAXIMUM] = max(answer[MAXIMUM], cur);
+    answer[MINIMUM] = min(answer[MINIMUM], cur);
+  }
+
+  FOR(NUM_OPERATORS) {
+    if (gOpCnt[i] > 0){
+      gOpCnt[i]--;
+      findAnswer(answer, gOperators[i](cur, gSeq[idx]), idx+1);
+      gOpCnt[i]++;
+    }
+  }
+}
+
+void solve() {
+  int answer[2];
+  answer[MAXIMUM] = -INF;
+  answer[MINIMUM] = INF;
+  findAnswer(answer, gSeq[0], 1);
+  printf("%d\n%d\n", answer[MAXIMUM], answer[MINIMUM]);
+}
+
+int main() {
+  getInput();
+  solve();
+  return 0;
 }
