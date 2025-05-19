b4f6b24395aeadd8409cee1e8c1518878acfc1c8
donggyu <53461960+leedonggyu1848@users.noreply.github.com>
2025-05-19 19:31:11 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 1320 ms, Memory: 241380 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
@@ -0,0 +1,51 @@
+import sys
+import heapq
+import math
+input = sys.stdin.readline
+push = heapq.heappush
+pop = heapq.heappop
+
+nv, ne, start, end, budget = map(int, input().split())
+edges = [list() for _ in range(nv+1)]
+costs = set()
+for _ in range(ne):
+    v1, v2, cost = map(int, input().split())
+    edges[v1].append((cost, v2))
+    edges[v2].append((cost, v2))
+    costs.add(cost)
+costs = sorted(list(costs))
+
+def dijkstra(start, limit):
+    global nv, edges
+    dist = [math.inf] * (nv+1)
+    q = []
+    push(q, (0, start))
+    dist[start] = 0
+    while q:
+        cur_cost, cur = pop(q)
+        if cur_cost != dist[cur]:
+            continue
+        for edge in edges[cur]:
+            cost, nxt = edge
+            if cost > limit:
+                continue
+            if dist[nxt] > cost + dist[cur]:
+                dist[nxt] = cost+dist[cur]
+                push(q, (dist[nxt], nxt))
+    return dist
+
+rst = math.inf
+def bsearch(left, right):
+    global rst, end, start, costs, budget
+    if left > right:
+        return
+    mid = (left + right) // 2
+    cost = dijkstra(start, costs[mid])[end]
+    if cost > budget:
+        bsearch(mid+1, right)
+    else:
+        rst = min(rst, costs[mid])
+        bsearch(left, mid-1)
+bsearch(0, len(costs)-1)
+print(rst if rst != math.inf else -1)
+
