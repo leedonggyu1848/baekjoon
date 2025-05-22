f7f6e565decbe70e72209ba44cc23582a0ba1106
이동규 <53461960+leedonggyu1848@users.noreply.github.com>
2025-05-22 10:37:05 +0900

    [Gold IV] Title: N-Queen, Time: 5840 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -2,21 +2,36 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+cnd = (1 << n) - 1
+rst = 0
 
-def next_q(x, row, pdg, ndg):
-    rst = 0
-    c_row = 1
-    c_pdg = 1 << x
-    c_ndg = 1 << (x+n)
-    for y in range(n):
-        if not ((row & c_row) or (pdg & c_pdg) or (ndg & c_ndg)):
-            if x == n-1:
-                return 1
-            else:
-                rst += next_q(x+1, row ^ c_row, pdg ^ c_pdg, ndg ^ c_ndg)
-        c_row = c_row << 1
-        c_pdg = c_pdg << 1
-        c_ndg = c_ndg >> 1
-    return rst
+# %s: % of y
+def n_queen(x, ys, pdgs, ndgs):
+    global n, rst, cnd
+    if x == n:
+        rst += 1
+# ys, pdgs, ndgs에서 사용한 위치의 합집합
+# cnd(후보자)에서 제거
+    y_cnd = cnd & ~(ys | pdgs | ndgs)
+    while y_cnd:
+        mask = -y_cnd
+# 오른쪽 끝 후보자 추출
+        y = y_cnd & mask
+        n_queen(x+1, ys|y, (pdgs|y) << 1, (ndgs|y) >> 1)
+        y_cnd ^= y
 
-print(next_q(0, 0, 0, 0))
+# 대칭성을 이용 가운데를 기준으로 대칭
+# => y의 반만 계산하면 됨
+half = n >> 1
+for y in range(half):
+    ys = 1 << y
+# 양의 대각선(증가함수)는 x, y에 있다면 x+1에서는 y+1을 차지당함
+# 음의 대각선(감소함수)는 x, y에 있다면 x+1에서는 y-1을 차지당함
+    n_queen(1, ys, ys << 1, ys >> 1)
+rst *= 2
+
+# 홀수면 y==half인 경우 계산하여 추가
+if n % 2:
+    ys = 1 << half
+    n_queen(1, ys, ys << 1, ys >> 1)
+print(rst)
