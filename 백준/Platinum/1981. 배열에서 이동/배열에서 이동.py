d664b6c70ca5a98ac77cd5257366179d1bfbdf4e
이동규 <53461960+leedonggyu1848@users.noreply.github.com>
2025-05-29 14:18:46 +0900

    [Platinum V] Title: 배열에서 이동, Time: 1092 ms, Memory: 37144 KB -BaekjoonHub

---
+++ 백준/Platinum/1981. 배열에서 이동/배열에서 이동.py
@@ -0,0 +1,63 @@
+import sys
+import math
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+board = []
+nums = set()
+for _ in range(n):
+    lst = list(map(int, input().split()))
+    board.append(lst)
+    nums.update(lst)
+
+maximum = max(nums)
+minimum = min(nums)
+
+lo = abs(board[0][0] - board[n-1][n-1])
+hi = maximum - minimum
+
+def bfs(left, right) -> bool:
+
+    def is_visitable(y, x):
+        if y < 0 or y >= n or x < 0 or x >= n:
+            return False
+
+        if board[y][x] > right or board[y][x] < left:
+            return False
+        return True
+
+    visited = [[False] * n for _ in range(n)]
+    dy = [0, 0, 1, -1]
+    dx = [1, -1, 0, 0]
+    q = deque()
+    if is_visitable(0, 0) and not visited[0][0]:
+        q.append((0, 0))
+        visited[0][0] = True
+    while q:
+        y, x = q.popleft()
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if is_visitable(ny, nx) and not visited[ny][nx]:
+                if ny == n-1 and nx == n-1:
+                    return True
+                visited[ny][nx] = True
+                q.append((ny, nx))
+    return False
+
+rst = math.inf
+while lo <= hi:
+    mid = (lo + hi) // 2
+    found = False
+    for left in range(minimum, maximum - mid + 1):
+        if bfs(left, left + mid):
+            found = True
+            break
+    if found:
+        rst = min(rst, mid)
+        hi = mid - 1
+    else:
+        lo = mid + 1
+print(rst)
+
