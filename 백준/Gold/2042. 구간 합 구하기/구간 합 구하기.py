69a5818a91b77db6c035ebd39439044bec6d8635
이동규 <53461960+leedonggyu1848@users.noreply.github.com>
2025-05-30 17:28:42 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1412 ms, Memory: 143312 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,49 +1,64 @@
 import sys
+input = sys.stdin.readline
 
-nums = [0]
+def left_child(idx):
+    return idx * 2 + 1
 
-class Solve():
-    def __init__(self, n, nums):
-        self.n = n
-        self.nums = nums
-        self.sgm = [0 for _ in range(n*4)]
-        def inner(node, start, end):
-            if start == end:
-                self.sgm[node] = self.nums[start]
-                return self.sgm[node]
-            mid = (start+end) // 2
-            self.sgm[node] = inner(node*2, start, mid) + inner(node*2+1, mid+1, end)
-            return self.sgm[node]
-        inner(1, 1, self.n)
+def right_child(idx):
+    return idx * 2 + 2
 
-    def sum(self, left, right):
-        def inner(start, end, node):
-            if left > end or right < start: return 0
-            if left <= start and right >= end: return self.sgm[node]
-            mid = (start+end) // 2
-            return inner(start, mid, node*2) + inner(mid+1, end, node*2+1)
+class SegTree:
+    def __init__(self, emts):
+        def _inner(left, right, node):
+            if left == right:
+                self.tree[node] = emts[left]
+                return self.tree[node]
+            mid = (left + right) // 2
+            left_v = _inner(left, mid, left_child(node))
+            right_v = _inner(mid+1, right, right_child(node))
+            self.tree[node] = left_v + right_v
+            return self.tree[node]
 
-        return inner(1, self.n, 1)
+        self.root = 0
+        self.n = len(emts)
+        self.tree = [0] * (self.n*4) # 보통 4배로 설정
+        # 가장 큰 인덱스 범위(트리 인덱스는 아님), 첫번째 트리 인덱스
+        _inner(0, self.n-1, self.root)
 
-    def update(self, index, diff):
-        def inner(start, end, node):
-            if index < start or index > end: return
-            self.sgm[node] += diff
-            if start == end: return
-            mid = (start+end) // 2
-            inner(start, mid, node*2)
-            inner(mid+1, end, node*2+1)
-        inner(1, self.n, 1)
+    def search(self, lo, hi):
+        def _inner(left, right, node):
+            # 탐색하는 범위 벗어남 (0은 항등원)
+            if hi < left or right < lo:
+                return 0
+            # 탐색 범위 안에 들어옴
+            if lo <= left and right <= hi:
+                return self.tree[node]
+            mid = (left + right) // 2
+            return _inner(left, mid, left_child(node)) + _inner(mid+1, right, right_child(node))
 
+        # 가장 큰 인덱스 범위(트리 인덱스는 아님), 첫번째 트리 인덱스
+        return _inner(0, self.n-1, self.root)
 
-n, m, k = map(int, sys.stdin.readline().split(' '))
-for _ in range(n):
-    nums.append(int(sys.stdin.readline()))
-solve = Solve(n, nums)
+    def update(self, tar, v):
+        def _inner(left, right, node):
+            if tar < left or right < tar:
+                return
+            self.tree[node] += v
+            if left == right:
+                return
+            mid = (left + right) // 2
+            _inner(left, mid, left_child(node))
+            _inner(mid+1, right, right_child(node))
+
+        _inner(0, self.n-1, self.root)
+
+n, m, k = map(int, input().split())
+nums = [int(input()) for _ in range(n)]
+tree = SegTree(nums)
 for _ in range(m + k):
-    case, bf, af = map(int, sys.stdin.readline().split(' '))
-    if case == 1:
-        solve.update(bf, af - nums[bf])
-        nums[bf] = af
-    elif case == 2:
-        print(solve.sum(bf, af))
+    t, a, b = map(int, input().split())
+    if t == 1:
+        tree.update(a-1, b - nums[a-1])
+        nums[a-1] = b
+    elif t == 2:
+        print(tree.search(a-1, b-1))
