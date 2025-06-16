debe8509e922312670f5971201066088e46aeb8e
이동규 <53461960+leedonggyu1848@users.noreply.github.com>
2025-06-16 12:42:59 +0900

    [Platinum IV] Title: 컨닝, Time: 988 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Platinum/1014. 컨닝/컨닝.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    sy, sx = map(int, input().split())
+    board = [[True] * sx for _ in range(sy)]
+    dp = [[-1] * (1 << sy) for _ in range(sx)] # x X 이전 열 위치들
+    for y in range(sy):
+        s = input()
+        for x in range(sx):
+            if s[x] =='x':
+                board[y][x] = False
+
+    def mask(prev, x):
+        mask = 0
+        for y in range(sy):
+            if not board[y][x]:
+                mask = mask | (1 << y)
+        mask = mask | prev | (prev >> 1) | (prev << 1)
+        return mask
+
+    def dfs(x, prev):
+        nonlocal dp
+        if x == sx:
+            return 0
+        if dp[x][prev] != -1:
+            return dp[x][prev]
+        dp[x][prev] = 0
+        m = mask(prev, x)
+        for cur in range(1 << sy):
+            if not (cur & m):
+                dp[x][prev] = max(dfs(x+1, cur) + cur.bit_count(), dp[x][prev])
+        return dp[x][prev]
+
+    m = mask(0, 0)
+    for cur in range(1 << sy):
+        if not (cur & m):
+            dp[0][0] = max(dfs(1, cur) + cur.bit_count(), dp[0][0])
+
+    print(dp[0][0])
+
+t = int(input())
+while t:
+    solve()
+    t -= 1
