f2f060abc9024d2c79bd5484bc388fd510c22c87
이동규 <53461960+leedonggyu1848@users.noreply.github.com>
2025-07-09 09:32:32 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 200 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+my, mx, width, k = map(int, input().split())
+stars = []
+stars_x = []
+stars_y = []
+for _ in range(k):
+    x, y = map(int, input().split())
+    stars.append((y, x))
+    stars_y.append(y)
+    stars_x.append(x)
+
+stars_y.sort()
+stars_x.sort()
+n_stars = len(stars)
+
+rst = 0
+for y in stars_y:
+    for x in stars_x:
+        cnt = 0
+        for star in stars:
+            if y <= star[0] <= y + width and x <= star[1] <= x + width:
+                cnt += 1
+        rst = max(cnt, rst)
+
+print(n_stars - rst)
